8c7b76e6af3f6c7fa089286591942ce45c677acc
Chris Xiao <csx773@gmail.com>
2019-03-20 08:32:03 +0000

    Initial commit on Javascripting folder

---
+++ for-loop.js
@@ -0,0 +1,11 @@
+//for (var i = 0; i < 10; i++) {
+       // log the numbers 0 through 9
+//       console.log(i)
+ //    }
+
+var total = 0;
+var limit = 10;
+for (var i = 0; i< limit; i++){
+  total += i;
+}
+console.log(total);
